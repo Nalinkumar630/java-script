e71082869e604107257bec2428fc17bf6cf336f6
Nalinkumar <rajulanalinkumar@gmail.com>
2025-09-22 11:00:08 +0530

    nalin

---
+++ app.js
@@ -0,0 +1,20 @@
+//types of variables
+//var, let, const
+//var - global variable
+//left const - local variable
+var a = 67;
+var a = 90;
+let b = 68;
+
+const c = 78;
+
+console.log("value b is ${b}");
+console.log("value c is ${c}");
+console.log(a);
+
+//comment -
+//single line comment
+//data types
+//1.primitive 2.non-primitive
+//primive - number, string, boolean, null, undefined, symbol
+//non-primitive - object, array
---
+++ datatype.js
@@ -0,0 +1,8 @@
+a = 7897;
+console.log(typeof a);
+name = "rajula";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
