50a23fe247f7a5f019df99d6ecaa371546b79e2e
Nalinkumar <rajulanalinkumar@gmail.com>
2025-09-22 16:07:02 +0530

    nalin

--- predefined.js
+++ predefined.js
@@ -1,3 +1,15 @@
 var a =Number(prompt("Enter value a "));
 var b = Number(prompt("Enter value b "));
-document.write(alert(a+b));
+document.write('Entered values a and b is ${a},${b} ');
+function add() {
+    alert(a+b);
+}
+function sub() {
+    alert(a-b);
+}
+function mul() {
+    alert(a*b);
+}
+function div() {
+    alert(a/b);
+}
