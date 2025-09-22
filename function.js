c7536cc09cc902773d435722929b993119308edb
Nalinkumar <rajulanalinkumar@gmail.com>
2025-09-22 12:37:37 +0530

    nalin

---
+++ function.js
@@ -0,0 +1,36 @@
+//function - code res
+//code resuability
+// function add() {
+//     var a = 67;
+//     var b = 78;
+//     document .write(a + b);
+//     console.log(a + b);
+
+// }
+
+// add();
+// ES6
+// const add = () => {
+//     var a =89;
+//     var b = 90;
+//     document.write(a + b);
+//     console.log(a + b);
+// };
+// add();
+
+// ES7
+
+// const add = (a, b) => console.log(a + b);
+// add(78,90);
+//hosting - to declare the variable to the top
+// console.log(b);
+// b = 45;
+// let name = "john";
+
+function demo() {
+    let name = "nalin";
+    document.write('hello, my name is ${name}');
+}
+document.write('hello, my name is ${name}');
+demo();
+
