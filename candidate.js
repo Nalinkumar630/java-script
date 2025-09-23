7056b1948e406df87b2c102054d1d159e5243ee1
Nalinkumar <rajulanalinkumar@gmail.com>
2025-09-23 12:08:23 +0530

    nalin

---
+++ candidate.js
@@ -0,0 +1,9 @@
+a = prompt("enter the age");
+if(a < 18) {
+document.write("you are a child");
+} else (a >= 18 && a < 60) {
+    document.write("you are an adult");
+} 
+elif(a >= 60) {
+    document.write("you are a senior citizen");
+}
